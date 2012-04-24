89da91ffbcbba9b1209cd0f8a33311d58c22576a
Jen Fong-Adwent <jen@ednapiranha.com>
2012-04-24 13:28:36 -0400

    remove csrf; not needed at this phase

--- routes/auth.js
+++ routes/auth.js
@@ -1,17 +1,8 @@
 var auth = require('../lib/authenticate');
 
 module.exports = function(app, settings) {
-  // Csrf check filter for pages with forms
-  var checkCSRF = function(req, res, next) {
-    if (req.session._csrf !== req.body._csrf) {
-      res.redirect('/');
-    } else {
-      next();
-    }
-  };
-
   // Login
-  app.post('/login', checkCSRF, function(req, res) {
+  app.post('/login', function(req, res) {
     auth.verify(req, settings, function(error, email) {
       if (email) {
         req.session.email = email;
